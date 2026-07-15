3e71eabb3d5987926310eb37a3ba762bd50f053f
Sai_KumarG <grkvp.sai@gmail.com>
2026-07-15 09:19:19 +0530

    daily problems

--- Leetcode_problems/leetcode-3658.py
+++ Leetcode_problems/leetcode-3658.py
@@ -4,7 +4,9 @@ class Solution(object):
             while b!=0:
                 a,b=b,a%b
             return a
-        even=n*(n+1)
-        odd=n**2
-        return (hcf(even,odd))
-print(Solution().gcdOfOddEvenSums(int(input())))
+        even=n*(n+1)#sum of n even natural numbers 
+        odd=n**2#sum of n odd natural numbers 
+        return (hcf(even,odd))#gcd/hcf of those two numbers(sum of even and sum of odd)
+n=int(input())
+#n=4(should be a natural number)
+print(Solution().gcdOfOddEvenSums(n))
